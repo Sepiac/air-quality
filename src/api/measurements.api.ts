2924de40b06b9b8a8feacbfc993281b30ff42aae
Dane Russell <danerussell4@gmail.com>
2021-11-11 17:08:49 -0500

    feat(api): add measurements api call resolves gh-2

---
+++ src/api/measurements.api.ts
@@ -0,0 +1,48 @@
+import getOpenAQResponse from './openAQ.api';
+export interface MeasurementsResponse {
+  meta: Meta;
+  results: Result[];
+}
+
+interface Result {
+  locationId: number;
+  location?: any;
+  parameter: string;
+  value: number;
+  date: Date;
+  unit: string;
+  coordinates: Coordinates;
+  country: string;
+  city: string;
+  isMobile: boolean;
+  isAnalysis: boolean;
+  entity: string;
+  sensorType: string;
+}
+
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+interface Date {
+  utc: string;
+  local: string;
+}
+
+interface Meta {
+  name: string;
+  license: string;
+  website: string;
+  page: number;
+  limit: number;
+  found: number;
+}
+
+
+const getMeasurements = async ():Promise<MeasurementsResponse> => {
+  const response = await getOpenAQResponse<MeasurementsResponse>('/measurements')
+  return response
+};
+
+export default getMeasurements;
