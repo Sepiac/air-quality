8ebc18ff8e9ca9b0bdba00d7e5aead71e866c3ec
Dane Russell <danerussell4@gmail.com>
2021-11-11 20:58:45 -0500

    feat(api): add locations call

---
+++ src/api/locations.api.ts
@@ -0,0 +1,80 @@
+import getOpenAQResponse from './openAQ.api';
+export interface MeasurementsResponse {
+  meta: Meta;
+  results: Result[];
+}
+
+interface LocationsResponse {
+  meta: Meta;
+  results: Result[];
+}
+
+interface Result {
+  id: number;
+  city: string;
+  name?: string;
+  entity: string;
+  country: string;
+  sources: Source[];
+  isMobile: boolean;
+  isAnalysis: boolean;
+  parameters: Parameter[];
+  sensorType: string;
+  coordinates?: Coordinates;
+  lastUpdated: string;
+  firstUpdated: string;
+  measurements: number;
+  bounds?: number[];
+  manufacturers?: Manufacturer[];
+}
+
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+interface Parameter {
+  id: number;
+  unit: string;
+  count: number;
+  average: number;
+  lastValue: number;
+  parameter: string;
+  displayName: string;
+  lastUpdated: string;
+  parameterId: number;
+  firstUpdated: string;
+  manufacturers?: Manufacturer[];
+}
+
+interface Manufacturer {
+  modelName: string;
+  manufacturerName: string;
+}
+
+interface Source {
+  id: string;
+  url?: string;
+  name: string;
+  readme?: string;
+  data_avg_dur?: string;
+  organization?: string;
+  lifecycle_stage?: string;
+}
+
+interface Meta {
+  name: string;
+  license: string;
+  website: string;
+  page: number;
+  limit: number;
+  found: number;
+}
+
+
+const getLocations = async (options?: object):Promise<LocationsResponse> => {
+  const response = await getOpenAQResponse<LocationsResponse>('/locations', options)
+  return response
+};
+
+export default getLocations;
