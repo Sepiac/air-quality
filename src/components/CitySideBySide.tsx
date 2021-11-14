86fd200a0115de47a21faae00f21bc1191e2e004
Dane Russell <danerussell4@gmail.com>
2021-11-14 02:26:22 -0500

    refactor: correct lint warning

--- src/components/CitySideBySide.tsx
+++ src/components/CitySideBySide.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { CitiesResult } from '../api'
 import { Grid } from 'semantic-ui-react'
 import { CityViewer, CityPicker } from '.'
