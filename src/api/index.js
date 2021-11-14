6940744997f380e9c54da4413284f5131307129b
Dane Russell <danerussell4@gmail.com>
2021-11-13 23:34:58 -0500

    feat(api): expor types

--- src/api/index.js
+++
@@ -1,3 +0,0 @@
-export { default as getCities } from './cities.api'
-export { default as getLocations } from './locations.api'
-export { default as getMeasurements } from './measurements.api'
---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+export { default as getCities } from './cities.api'
+export type { CitiesResponse } from './cities.api'
+
+export { default as getLocations } from './locations.api'
+export type { LocationsResponse } from './locations.api'
+
+export { default as getMeasurements } from './measurements.api'
+export type { MeasurementsResponse } from './measurements.api'
